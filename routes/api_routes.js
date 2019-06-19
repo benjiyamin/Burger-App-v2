c0c7b544c7fb498677ab8001177a400b548f107d
Ben Miller <mail@benkmiller.com>
2019-06-19 19:43:18 -0400

    Setup customer api routes

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -6,59 +6,66 @@ const db = require('../models')
 const router = express.Router()
 
 
-router.get('/api/burgers', function (request, response) {
-  db.Burger.findAll({})
-    .then(data => {
-      response.json(data)
-    })
-    .catch(() => {
-      response.status(500).end()
-    })
-})
+function createRoutes(model, baseUrl, includeModel) {
+
+  router.get(baseUrl, function (request, response) {
+    let options = {}
+    if (includeModel) options.include = [includeModel]
+    model.findAll(options)
+      .then(data => {
+        response.json(data)
+      })
+      .catch(() => {
+        response.status(500).end()
+      })
+  })
 
 
-router.post('/api/burgers', function (request, response) {
-  db.Burger.create(request.body)
-    .then(data => {
-      response.json(data)
-      //response.status(204).end()
-    })
-    .catch(() => {
-      response.status(500).end()
-    })
-})
+  router.post(baseUrl, function (request, response) {
+    model.create(request.body)
+      .then(data => {
+        response.json(data)
+      })
+      .catch(() => {
+        response.status(500).end()
+      })
+  })
 
 
-router.put('/api/burgers/:id', function (request, response) {
-  db.Burger.update(request.body, {
-      where: {
-        id: request.params.id
-      }
-    })
-    .then(data => {
-      response.json(data)
-      //response.status(204).end()
-    })
-    .catch(() => {
-      response.status(500).end()
-    })
-})
+  router.put(`${baseUrl}/:id`, function (request, response) {
+    model.update(request.body, {
+        where: {
+          id: request.params.id
+        }
+      })
+      .then(data => {
+        response.json(data)
+      })
+      .catch(() => {
+        response.status(500).end()
+      })
+  })
 
 
-router.delete('/api/burgers/:id', function (request, response) {
-  db.Burger.destroy({
-      where: {
-        id: request.params.id
-      }
-    })
-    .then(() => {
-      //response.json(data)
-      response.status(204).end()
-    })
-    .catch(() => {
-      response.status(500).end()
-    })
-})
+  router.delete(`${baseUrl}/:id`, function (request, response) {
+    model.destroy({
+        where: {
+          id: request.params.id
+        }
+      })
+      .then(() => {
+        response.status(204).end()
+      })
+      .catch(() => {
+        response.status(500).end()
+      })
+  })
+
+}
+
+
+createRoutes(db.Customer, '/api/customers', db.Burger)
+createRoutes(db.Burger, '/api/burgers', db.Customer)
 
 
 module.exports = router
